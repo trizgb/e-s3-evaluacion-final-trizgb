cd17cd04abb20195ad8912fb1bf843499264be91
trizgb <beatrizgomez92@gmail.com>
2019-01-25 12:51:09 +0100

    Add docs folder

---
+++ docs/precache-manifest.8a1e8fb410c3c9774ada5740afa8ae56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f69c832b6ca2fc7737812f16872ee86c",
+    "url": "./static/media/hogwarts-castle-fog.f69c832b.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fd93065d8a73786de4a5",
+    "url": "./static/js/main.fd93065d.chunk.js"
+  },
+  {
+    "revision": "75dceaf7d4aa0ee98f02",
+    "url": "./static/js/1.75dceaf7.chunk.js"
+  },
+  {
+    "revision": "fd93065d8a73786de4a5",
+    "url": "./static/css/main.846f5d9f.chunk.css"
+  },
+  {
+    "revision": "0f8035cfa48be27d37ed6c3fbea3db69",
+    "url": "./index.html"
+  }
+];
--- src/services/HPService.js
+++ src/services/HPService.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'http://hp-api.herokuapp.com/api/characters';
+const ENDPOINT = 'https://hp-api.herokuapp.com/api/characters';
 
 const getCharacters = () => fetch(ENDPOINT).then(response => response.json());
 
